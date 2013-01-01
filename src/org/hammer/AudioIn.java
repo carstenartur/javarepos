45fbabe02058adc908c35847d84cc392f5f1bc88
chammer <chammer@Carsten-PC.fritz.box>
2013-01-01 18:15:46 +0100

    initial version

---
+++ src/org/hammer/AudioIn.java
@@ -0,0 +1,151 @@
+package org.hammer;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
+import javax.swing.JPanel;
+
+public final class AudioIn extends JPanel {
+    /**
+     * Tausendstel also jede Millisekunde ein Tick
+     */
+    public static final float TICKDISTANCE_IN_MS = 1f/1000f;
+
+    boolean stopped = false;
+    private TargetDataLine line;
+    private byte[] datas;
+    float sampleRate = 16000.0f;
+    int sampleSizeInBits = 8;
+    int channels = 2;
+    int tickeverynsample=(int) ((TICKDISTANCE_IN_MS)/(1f/sampleRate)); 
+    boolean signed = false;
+    boolean bigEndian = false;
+    private int[] xPoints;
+    private int[][] yPoints;
+    private int datasize;
+    private int divisor = 1;
+    AudioFormat format;
+
+    public int getDivisor() {
+        return divisor;
+    }
+
+    public void setDivisor(int divisor) {
+        this.divisor = divisor;
+        computedatasize();
+    }
+
+    public int getDatasize() {
+        return datasize;
+    }
+
+    public AudioIn() {
+        super(true);
+
+        format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+        line = null;
+        DataLine.Info info = new DataLine.Info(TargetDataLine.class,
+                format); // format is an AudioFormat object
+        if (!AudioSystem.isLineSupported(info)) {
+            // Handle the error ... 
+        }
+        // Obtain and open the line.
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException ex) {
+            // Handle the error ... 
+        }
+        computedatasize();
+
+        javax.swing.Timer t = new javax.swing.Timer(200, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repaint();
+            }
+        });
+        t.start();
+        final Thread thread = new Thread(processing);
+        thread.start();
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                recomputexvalues();
+            }
+        });
+    }
+
+    public void recomputexvalues() {
+        for (int i = 0; i < datasize/channels; i++) {
+            xPoints[i] = (getWidth() * i) / datasize*channels;
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
+        g.setXORMode(Color.yellow);
+        g.drawPolyline(xPoints, yPoints[0], datasize/channels);
+        g.setXORMode(Color.cyan);
+        g.drawPolyline(xPoints, yPoints[1], datasize/channels);
+        g.setXORMode(Color.red);
+   //     g.setColor(Color.red);
+        g.drawLine(0, getHeight()/2, getWidth() - 1, getHeight()/2);
+         for (int i = 0; i < datasize/channels; i=i+tickeverynsample) {
+             g.drawLine(xPoints[i], getHeight()/2, xPoints[i], getHeight()/2+6);
+        }
+    }
+    
+    Runnable processing = new Runnable() {
+        @Override
+        public void run() {
+            // Assume that the TargetDataLine, line, has already
+            // been obtained and opened.
+//		ByteArrayOutputStream out  = new ByteArrayOutputStream();
+            int numBytesRead;
+
+
+            // Begin audio capture.
+            line.start();
+
+            // Here, stopped is a global boolean set by another thread.
+            while (!stopped) {
+                // Read the next chunk of data from the TargetDataLine.
+                try{
+                numBytesRead = line.read(datas, 0, datas.length);
+                // Save this chunk of data.
+
+                for (int i = 0; i < datasize/channels; i++) {
+                    int j = (getHeight() * (datas[i*channels] & 0xFF)) / 256;
+                    yPoints[0][i] = j;
+                    int k = (getHeight() * (datas[i*channels+1] & 0xFF)) / 256;
+                    yPoints[1][i] = k;
+//				System.out.println(yPoints[i]);
+                }
+                }catch(Exception ex){
+                    
+                }
+            }
+        }
+    };
+
+    public void computedatasize() {
+        //		int bufferSize = (int)format.getSampleRate() * 
+        //			    format.getFrameSize();
+        //			  byte buffer[] = new byte[bufferSize];
+        datasize = line.getBufferSize() / divisor;
+      //  if((datasize%2)>0)datasize=+1;
+        datas = new byte[datasize];
+        xPoints = new int[datasize/channels];
+        yPoints = new int[2][datasize/channels];
+    }
+}
---
+++ src/org/hammer/AudioWindow.java
@@ -0,0 +1,166 @@
+package org.hammer;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.GridLayout;
+
+public class AudioWindow extends JFrame {
+
+    private JPanel contentPane;
+    private final Action action = new SwingAction();
+    private JTextField textFielddatasize;
+    private JTextField textFielddivisor;
+    private JTextField audioformat;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    AudioWindow frame = new AudioWindow();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public AudioWindow() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 512, 406);
+
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu mnFile = new JMenu("File");
+        menuBar.add(mnFile);
+
+        JMenuItem mntmStart = new JMenuItem("Start");
+        mntmStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+        mnFile.add(mntmStart);
+        menuBar.add(Box.createGlue());
+
+        JMenu mnHelp = new JMenu("Help");
+        menuBar.add(mnHelp);
+
+        JMenuItem mntmAbout = new JMenuItem("About");
+        mntmAbout.setAction(action);
+        mnHelp.add(mntmAbout);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        JPanel textfelder = new JPanel();
+        contentPane.add(textfelder, BorderLayout.NORTH);
+        textfelder.setLayout(new GridLayout(3, 2, 0, 0));
+
+        JLabel lblDatasize = new JLabel("datasize");
+        textfelder.add(lblDatasize);
+
+        textFielddatasize = new JTextField();
+        textFielddatasize.setEnabled(false);
+        textFielddatasize.setEditable(false);
+        textfelder.add(textFielddatasize);
+        textFielddatasize.setColumns(10);
+
+        JScrollPane scrollPane = new JScrollPane();
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+        final AudioIn panel = new AudioIn();
+        scrollPane.setViewportView(panel);
+
+
+        textFielddatasize.setText("" + panel.getDatasize());
+        
+        JLabel label = new JLabel("");
+        textfelder.add(label);
+        
+        JLabel label_1 = new JLabel("");
+        textfelder.add(label_1);
+                
+                        JLabel lblDivisor = new JLabel("divisor");
+                        textfelder.add(lblDivisor);
+        
+                textFielddivisor = new JTextField();
+                textFielddivisor.setEnabled(false);
+                textFielddivisor.setEditable(false);
+                textfelder.add(textFielddivisor);
+                textFielddivisor.setColumns(10);
+                textFielddivisor.setText("" + panel.getDivisor());
+        
+        JLabel label_2 = new JLabel("");
+        textfelder.add(label_2);
+        
+        JLabel label_3 = new JLabel("");
+        textfelder.add(label_3);
+        
+        JLabel lblAudioformat = new JLabel("audioformat");
+        textfelder.add(lblAudioformat);
+        
+        audioformat = new JTextField();
+        audioformat.setHorizontalAlignment(SwingConstants.CENTER);
+        audioformat.setEnabled(false);
+        audioformat.setEditable(false);
+        textfelder.add(audioformat);
+        audioformat.setColumns(30);
+        audioformat.setText(panel.format.toString());
+        
+        JSlider slider = new JSlider();
+        slider.setMinimum(1);
+        slider.setValue(1);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int value = ((JSlider) e.getSource()).getValue();
+                panel.setDivisor(value);
+                panel.recomputexvalues();
+                textFielddivisor.setText("" + value);
+                contentPane.repaint();
+            }
+        });
+        contentPane.add(slider, BorderLayout.SOUTH);
+    }
+
+    private class SwingAction extends AbstractAction {
+
+        public SwingAction() {
+            putValue(NAME, "About");
+            putValue(SHORT_DESCRIPTION, "Some short description");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "Testversion");
+        }
+    }
+}
